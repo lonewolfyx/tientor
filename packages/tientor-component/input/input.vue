7daf3e5b5486489c060bdd36e5a3d8c2ace051a6
lonewolfyx <877085893@qq.com>
2024-11-22 17:30:14 +0800

    fix

--- packages/tientor-component/input/input.vue
+++ packages/tientor-component/input/input.vue
@@ -6,35 +6,8 @@
 
 <script setup>
 import {useAsyncComponent} from "../_hooks/useAsyncComponent.js";
-import {ElInput} from "element-plus";
 
 const component = useAsyncComponent('Input');
-
-const div = h(ElInput);
-// const ui = getUiFramework();
-
-// const InputComponent = ref(null);
-//
-// onBeforeMount(async () => {
-//     if (ui === 'element') {
-//         const ElInput = await import('element-plus').then((module) => module.ElInput);
-//         InputComponent.value = markRaw(ElInput);
-//         // 动态加载Element Plus的样式文件
-//         import('element-plus/dist/index.css');
-//     } else if (ui === 'arco') {
-//         const ArcoInput = await import('@arco-design/web-vue').then((module) => module.Input);
-//         InputComponent.value = markRaw(ArcoInput);
-//         // 动态加载Arco Design的样式文件
-//         import('@arco-design/web-vue/dist/arco.css');
-//     } else if (ui === 'antdv') {
-//         const AntdInput = await import('ant-design-vue').then((module) => module.Input);
-//         InputComponent.value = markRaw(AntdInput);
-//         // 动态加载Ant Design Vue的样式文件
-//         import('ant-design-vue/dist/reset.css');
-//     } else {
-//         console.error('Unsupported UI framework:', ui);
-//     }
-// });
 
 defineOptions({
     name: 'TientorInput'
