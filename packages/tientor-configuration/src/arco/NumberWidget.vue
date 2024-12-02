979b6688a1699a7f4c79fd092cadc75dff0c140a
lonewolfyx <877085893@qq.com>
2024-12-02 22:08:04 +0800

    feat：新增 Arco 数字输入框配置项

--- packages/tientor-configuration/src/arco/NumberWidget.vue
+++ packages/tientor-configuration/src/arco/NumberWidget.vue
@@ -1,9 +1,65 @@
 <template>
-
+    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>
+    <DividerHeader title="组件设置"/>
+    <a-form-item label="默认值：" field="defaultValue">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].defaultValue"
+            placeholder="请输入默认内容..."
+            :min="0"
+            model-event="input"
+        />
+    </a-form-item>
+    <a-form-item label="提示文字：" field="placeholder">
+        <a-input v-model="queryForm._widgetProp[ui].placeholder" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="按钮模式：">
+        <a-radio-group type="button" v-model="queryForm._widgetProp[ui].mode">
+            <a-radio value="embed">按钮内嵌</a-radio>
+            <a-radio value="button">左右按钮</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="允许清空输入框：" field="allowClear">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowClear"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="只读状态：" field="readonly">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].readonly"/>
+    </a-form-item>
+    <a-form-item label="隐藏按钮：" field="hideButton">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].hideButton"/>
+    </a-form-item>
+    <a-form-item label="最小值：" field="min">
+        <a-input-number v-model="queryForm._widgetProp[ui].min" :min="0" placeholder="请输入最小值..." mode="button"/>
+    </a-form-item>
+    <a-form-item label="最大值：" field="max">
+        <a-input-number v-model="queryForm._widgetProp[ui].max" :min="0" placeholder="请输入最大值..." mode="button"/>
+    </a-form-item>
+    <a-form-item label="数字精度：" field="precision">
+        <a-input-number v-model="queryForm._widgetProp[ui].precision" :min="0" mode="button"/>
+    </a-form-item>
+    <a-form-item label="数字变化步长：" field="step">
+        <a-input-number v-model="queryForm._widgetProp[ui].step" :min="0" mode="button"/>
+    </a-form-item>
 </template>
 
 <script setup>
+
+import TientorConfigArcoFormItem from "./FormItem.vue";
+import DividerHeader from "../components/DividerHeader.vue";
+
 defineOptions({
     name: 'TientorConfigArcoNumber'
 })
+
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex'])
 </script>
--- packages/tientor-widget/src/widget/NumberWidget.ts
+++ packages/tientor-widget/src/widget/NumberWidget.ts
@@ -1,7 +1,34 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps = {
+    // 默认值
+    // defaultValue: 0,
+    // 按钮模式
+    mode: 'embed',
+    // 数字精度
+    precision: 0,
+    // 数字变化步长
+    step: 1,
+    // 输入框大小
+    size: 'medium',
+    // 是否禁用
+    disabled: false,
+    // 最大值
+    max: 10,
+    // 最小值
+    min: 0,
+    // 提示文本
+    placeholder: '',
+    // 是否隐藏按钮
+    hideButton: false,
+    // 是否允许清空输入框
+    allowClear: false,
+    // 是否为只读状态
+    readonly: false,
+}
 export const NumberWidget: WidgetInterface = {
+    id: 'dc7a6cdbaf8b41679d1d3a3f7d0c8599',
     name: '数字输入框',
     icon: 'widget-number',
     tag: 'number',
@@ -10,6 +37,10 @@ export const NumberWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
+    // 配置面板
+    _config: {
+        arco: 'TientorConfigArcoNumber',
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -17,5 +48,7 @@ export const NumberWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
