a7ece56d210f447738ec07eca33c1c34eb72dc8c
lonewolfyx <877085893@qq.com>
2024-12-02 21:41:11 +0800

    feat：新增 Arco 密码配置项

--- packages/tientor-configuration/src/arco/PasswordWidget.vue
+++ packages/tientor-configuration/src/arco/PasswordWidget.vue
@@ -1,9 +1,53 @@
 <template>
-
+    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>
+    <DividerHeader title="组件设置"/>
+    <a-form-item label="默认值：" field="defaultValue">
+        <a-input v-model="queryForm._widgetProp[ui].defaultValue" placeholder="请输入默认内容..."/>
+    </a-form-item>
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="允许清空输入框：" field="allowClear">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowClear"/>
+    </a-form-item>
+    <a-form-item label="可见按钮：" field="invisibleButton">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].invisibleButton"/>
+    </a-form-item>
+    <a-form-item label="遮蔽密码：" field="visibility" help="关闭即显示密码内容"
+                 :disabled="!queryForm._widgetProp[ui].invisibleButton">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].visibility"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="提示文字：" field="placeholder">
+        <a-input v-model="queryForm._widgetProp[ui].placeholder" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="最大文本长度：" field="maxLength">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].maxLength"
+            placeholder="请输入默认提示文字..."
+            mode="button"
+        />
+    </a-form-item>
+    <a-form-item label="显示字数统计：" field="showWordLimit">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showWordLimit"/>
+    </a-form-item>
 </template>
 
 <script setup>
+import DividerHeader from "../components/DividerHeader.vue";
+import TientorConfigArcoFormItem from "./FormItem.vue";
+
 defineOptions({
     name: 'TientorConfigArcoPassword'
 })
+
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex'])
+
 </script>
--- packages/tientor-widget/src/widget/PasswordWidget.ts
+++ packages/tientor-widget/src/widget/PasswordWidget.ts
@@ -1,7 +1,29 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: '',
+    // 输入框大小
+    size: 'medium',
+    // 是否允许清空输入框
+    allowClear: false,
+    // 是否显示源内容
+    visibility: true,
+    // 是否禁用
+    disabled: false,
+    // 提示文字
+    placeholder: '',
+    // 输入值的最大长度
+    maxLength: 0,
+    // 是否显示字数统计
+    showWordLimit: false,
+    // 是否显示可见按钮
+    invisibleButton: false
+}
+
 export const PasswordWidget: WidgetInterface = {
+    id: 'e0b2862a2fd44eeb8eab562b843349f1',
     name: '密码输入框',
     icon: 'widget-password',
     tag: 'password',
@@ -10,6 +32,10 @@ export const PasswordWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
+    // 配置面板
+    _config: {
+        arco: 'TientorConfigArcoPassword',
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -17,5 +43,7 @@ export const PasswordWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
