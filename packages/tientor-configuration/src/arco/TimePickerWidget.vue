f0cb6765a5c17043bdb3ed28c17d8dcb8039d067
lonewolfyx <877085893@qq.com>
2024-12-03 16:30:25 +0800

    feat：新增 Arco TimePick 时间选择器配置项

--- packages/tientor-configuration/src/arco/TimePickerWidget.vue
+++ packages/tientor-configuration/src/arco/TimePickerWidget.vue
@@ -1,9 +1,84 @@
 <template>
-
+    <a-form-item label="类型：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].type" @change="typeChangeHandle">
+            <a-radio value="time">时间</a-radio>
+            <a-radio value="time-range">范围时间</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="允许清空输入框：" field="allowClear">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowClear"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="只读状态：" field="readonly">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].readonly"/>
+    </a-form-item>
+    <template v-if="queryForm._widgetProp[ui].type==='time-range'">
+        <a-form-item label="提示文字1：">
+            <a-input v-model="queryForm._widgetProp[ui].placeholder[0]" placeholder="请输入默认提示文字..."/>
+        </a-form-item>
+        <a-form-item label="提示文字2：">
+            <a-input v-model="queryForm._widgetProp[ui].placeholder[1]" placeholder="请输入默认提示文字..."/>
+        </a-form-item>
+    </template>
+    <template v-else>
+        <a-form-item label="提示文字：" field="placeholder">
+            <a-input v-model="queryForm._widgetProp[ui].placeholder" placeholder="请输入默认提示文字..."/>
+        </a-form-item>
+    </template>
+    <a-form-item label="12小时制：" field="use12Hours">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].use12Hours"/>
+    </a-form-item>
+    <a-form-item label="弹出框位置：">
+        <a-select v-model="queryForm._widgetProp[ui].position" :options="positionOptions"/>
+    </a-form-item>
+    <a-form-item label="隐藏时销毁：" field="unmountOnClose">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].unmountOnClose"/>
+    </a-form-item>
+    <a-form-item label="确定状态：" field="disableConfirm" help="开启后直接点选时间不需要点击确认按钮">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disableConfirm"/>
+    </a-form-item>
 </template>
 
 <script setup>
 defineOptions({
     name: 'TientorConfigArcoTimePicker'
 })
+
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex'])
+
+// 弹出框位置
+const positionOptions = [
+    {label: '顶部', value: 'top'},
+    {label: '顶部左侧', value: 'tl'},
+    {label: '顶部右侧', value: 'tr'},
+    {label: '底部', value: 'bottom'},
+    {label: '底部左侧', value: 'bl'},
+    {label: '底部右侧', value: 'br'}
+]
+
+onMounted(() => {
+    nextTick(() => {
+        typeChangeHandle(queryForm._widgetProp[ui].type)
+    })
+})
+// 类型切换
+const typeChangeHandle = (e) => {
+    if (e === 'time-range') {
+        queryForm._widgetProp[ui].defaultValue = [];
+        queryForm._widgetProp[ui].placeholder = ['开始时间', '结束时间'];
+    } else {
+        queryForm._widgetProp[ui].defaultValue = '';
+        queryForm._widgetProp[ui].placeholder = '开始时间';
+    }
+}
 </script>
--- packages/tientor-widget/src/widget/TimePickerWidget.ts
+++ packages/tientor-widget/src/widget/TimePickerWidget.ts
@@ -1,14 +1,48 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps = {
+    // 类型
+    type: 'time',
+    // 是否允许清空输入框
+    allowClear: false,
+    // 是否禁用
+    disabled: false,
+    // 是否为只读状态
+    readonly: false,
+    // 提示文字
+    placeholder: '',
+    // 输入框尺寸
+    size: 'medium',
+    // 12 小时制
+    use12Hours: false,
+    // 设置 时 / 分 / 秒 的选择间隔
+    step: {
+        hour: 1,
+        minute: 1,
+        second: 1
+    },
+    // 弹出的位置
+    position: 'bl',
+    // 禁用确认步骤，开启后直接点选时间不需要点击确认按钮
+    disableConfirm: false,
+    // 是否在关闭后销毁 dom 结构
+    unmountOnClose: false
+}
+
 export const TimePickerWidget: WidgetInterface = {
-    name: '时间选择器',
+    id: 'ef2d958472f74f029b351f0d0f6264d4',
+    name: '时间选择器-✔',
     icon: 'widget-time-picker',
     tag: 'timePicker',
     enabled: {
         element: true,
         antv: true,
         arco: true
+    },
+    // 配置面板
+    _config: {
+        arco: 'TientorConfigArcoTimePicker',
     },
     _itemProps: {
         arco: {
@@ -17,5 +51,7 @@ export const TimePickerWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
