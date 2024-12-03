fc391cc6a7a74cf573f215aa5da44b15a0c9e617
lonewolfyx <877085893@qq.com>
2024-12-03 17:16:39 +0800

    feat：新增 Arco Color 颜色选择器配置项

--- packages/tientor-configuration/src/arco/ColorWidget.vue
+++ packages/tientor-configuration/src/arco/ColorWidget.vue
@@ -1,9 +1,73 @@
 <template>
-
+    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>
+    <DividerHeader title="组件设置"/>
+    <a-form-item label="颜色格式：">
+        <a-radio-group type="button" v-model="queryForm._widgetProp[ui].format">
+            <a-radio value="hex">Hex</a-radio>
+            <a-radio value="rgb">Rgb</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="颜色值：" field="showText">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showText"/>
+    </a-form-item>
+    <a-form-item label="历史颜色：" field="showHistory">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showHistory"/>
+    </a-form-item>
+    <a-form-item label="预设颜色：" field="showHistory">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showPreset"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="禁用透明通道：" field="disabledAlpha">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabledAlpha"/>
+    </a-form-item>
+    <a-form-item label="颜色面板：" field="hideTrigger" help="开启后将仅显示颜色面板">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].hideTrigger"/>
+    </a-form-item>
+    <DividerHeader title="预设颜色的颜色组"/>
+    <a-row>
+        <a-col :span="12" v-for="(item,index) in queryForm._widgetProp[ui].presetColors" :key="item+index"
+               style="margin-bottom:10px">
+            <a-row>
+                <a-col :span="16">
+                    <a-color-picker v-model="queryForm._widgetProp[ui].presetColors[index]" showText/>
+                </a-col>
+                <a-col :span="5">
+                    <a-button shape="circle" @click="deleteHandle(index)">
+                        <icon-delete/>
+                    </a-button>
+                </a-col>
+            </a-row>
+        </a-col>
+    </a-row>
+    <a-button type="primary" long @click="addColors('presetColors')">新增预设颜色</a-button>
 </template>
 
 <script setup>
+import DividerHeader from "../components/DividerHeader.vue";
+import TientorConfigArcoFormItem from "./FormItem.vue";
+
 defineOptions({
     name: 'TientorConfigArcoColor'
 })
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex'])
+
+// 增加颜色
+const addColors = (type) => {
+    queryForm._widgetProp[ui][type].push('')
+}
+
+// 删除颜色
+const deleteHandle = (index) => {
+    queryForm._widgetProp[ui].presetColors.splice(index, 1)
+}
 </script>
--- packages/tientor-widget/src/widget/ColorWidget.ts
+++ packages/tientor-widget/src/widget/ColorWidget.ts
@@ -1,8 +1,57 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: '',
+    // 颜色值的格式
+    format: '',
+    // 尺寸
+    size: 'medium',
+    // 显示颜色值
+    showText: false,
+    // 显示历史颜色
+    showHistory: false,
+    // 显示预设颜色
+    showPreset: false,
+    // 禁用
+    disabled: false,
+    // 禁用透明通道
+    disabledAlpha: false,
+    // 没有触发元素，只显示颜色面板
+    hideTrigger: false,
+    // 历史颜色的颜色数组
+    historyColors: [],
+    // 预设颜色的颜色数组
+    presetColors: [
+        '#00B42A',
+        '#3C7EFF',
+        '#FF7D00',
+        '#F76965',
+        '#F7BA1E',
+        '#F5319D',
+        '#D91AD9',
+        '#9FDB1D',
+        '#FADC19',
+        '#722ED1',
+        '#3491FA',
+        '#7BE188',
+        '#93BEFF',
+        '#FFCF8B',
+        '#FBB0A7',
+        '#FCE996',
+        '#FB9DC7',
+        '#F08EE6',
+        '#DCF190',
+        '#FDFA94',
+        '#C396ED',
+        '#9FD4FD',
+    ]
+}
+
 export const ColorWidget: WidgetInterface = {
-    name: '颜色选择器',
+    id: 'c17cf4f97c754ce98887f73814721bf2',
+    name: '颜色选择器-✔',
     icon: 'widget-color',
     tag: 'color',
     enabled: {
@@ -10,7 +59,9 @@ export const ColorWidget: WidgetInterface = {
         antv: false,
         arco: true
     },
-    _config: {},
+    _config: {
+        arco: 'TientorConfigArcoColor'
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -18,5 +69,7 @@ export const ColorWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
