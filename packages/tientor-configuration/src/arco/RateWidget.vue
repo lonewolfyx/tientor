a6456f716a26b825fd741801a917b81962c659b8
lonewolfyx <877085893@qq.com>
2024-12-03 09:44:24 +0800

    feat：新增 Arco Rate 评分组件配置项

--- packages/tientor-configuration/src/arco/RateWidget.vue
+++ packages/tientor-configuration/src/arco/RateWidget.vue
@@ -1,9 +1,49 @@
 <template>
-
+    <!--    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>-->
+    <DividerHeader title="组件设置"/>
+    <a-form-item label="默认值：" field="defaultValue">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].defaultValue"
+            placeholder="请输入默认内容..."
+            :min="0"
+            :max="queryForm._widgetProp[ui].count"
+            mode="button"
+        />
+    </a-form-item>
+    <a-form-item label="评分的总数：" field="count">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].count"
+            placeholder="请输入评分数量..."
+            :min="0"
+            mode="button"
+        />
+    </a-form-item>
+    <a-form-item label="半选评分：" field="allowHalf">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowHalf"/>
+    </a-form-item>
+    <a-form-item label="允许清空评分：" field="allowClear">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowClear"/>
+    </a-form-item>
+    <a-form-item label="笑脸分级：" field="grading">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].grading"/>
+    </a-form-item>
+    <a-form-item label="只读状态：" field="readonly">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].readonly"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="评分颜色：" field="disabled">
+        <a-color-picker v-model="queryForm._widgetProp[ui].color" showText disabledAlpha/>
+    </a-form-item>
 </template>
 
 <script setup>
+import DividerHeader from "../components/DividerHeader.vue";
+
 defineOptions({
     name: 'TientorConfigArcoRate'
 })
+
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex'])
 </script>
--- packages/tientor-widget/src/widget/RateWidget.ts
+++ packages/tientor-widget/src/widget/RateWidget.ts
@@ -1,6 +1,24 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps: any = {
+    // 默认值
+    defaultValue: 0,
+    // 评分的总数
+    count: 5,
+    // 是否允许半选
+    allowHalf: false,
+    // 是否允许清除
+    allowClear: false,
+    // 是否开启笑脸分级
+    grading: false,
+    // 是否为只读状态
+    readonly: false,
+    // 是否禁用
+    disabled: false,
+    // 颜色
+    color: ''
+}
 export const RateWidget: WidgetInterface = {
     name: '评分',
     icon: 'widget-rate',
@@ -10,6 +28,10 @@ export const RateWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
+    // 配置面板
+    _config: {
+        arco: 'TientorConfigArcoRate',
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -17,5 +39,7 @@ export const RateWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
