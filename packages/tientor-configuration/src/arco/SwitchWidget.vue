dc5475600dbaf615fc90a28162142ba12e94d0ed
lonewolfyx <877085893@qq.com>
2024-12-04 15:13:32 +0800

    feat：新增 Arco Switch 开关配置项

--- packages/tientor-configuration/src/arco/SwitchWidget.vue
+++ packages/tientor-configuration/src/arco/SwitchWidget.vue
@@ -1,9 +1,52 @@
 <template>
-
+    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>
+    <DividerHeader title="组件设置"/>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="加载中状态：" field="loading">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].loading"/>
+    </a-form-item>
+    <a-form-item label="开关类型：">
+        <a-radio-group type="button" v-model="queryForm._widgetProp[ui].type">
+            <a-radio value="circle">圆形</a-radio>
+            <a-radio value="round">圆角</a-radio>
+            <a-radio value="line">线性</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="开关大小：">
+        <a-radio-group type="button" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="选中时的值：" field="checkedValue">
+        <a-input v-model="queryForm._widgetProp[ui].checkedValue" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="未选中时的值：" field="uncheckedValue">
+        <a-input v-model="queryForm._widgetProp[ui].uncheckedValue" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="选中颜色：" field="checkedColor">
+        <a-color-picker v-model="queryForm._widgetProp[ui].checkedColor" showText disabledAlpha/>
+    </a-form-item>
+    <a-form-item label="未选颜色：" field="uncheckedColor">
+        <a-color-picker v-model="queryForm._widgetProp[ui].uncheckedColor" showText disabledAlpha/>
+    </a-form-item>
+    <a-form-item label="打开文案：" field="checkedText" help="开关类型为线性和开关小型规模下不生效">
+        <a-input v-model="queryForm._widgetProp[ui].checkedText" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="关闭文案：" field="uncheckedText" help="开关类型为线性和开关小型规模下不生效">
+        <a-input v-model="queryForm._widgetProp[ui].uncheckedText" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
 </template>
 
 <script setup>
+
+import TientorConfigArcoFormItem from "./FormItem.vue";
+import DividerHeader from "../components/DividerHeader.vue";
+
 defineOptions({
     name: 'TientorConfigArcoSwitch'
 })
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex']);
 </script>
--- packages/tientor-widget/src/widget/SwitchWidget.ts
+++ packages/tientor-widget/src/widget/SwitchWidget.ts
@@ -1,8 +1,34 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: false,
+    // 是否禁用
+    disabled: false,
+    // 是否为加载中状态
+    loading: false,
+    // 开关的类型
+    type: 'circle',
+    // 开关的大小
+    size: 'medium',
+    // 选中时的值
+    checkedValue: '',
+    // 未选中时的值
+    uncheckedValue: '',
+    // 选中时的开关颜色
+    checkedColor: '#165dff',
+    // 未选中时的开关颜色
+    uncheckedColor: '#c9cdd4',
+    // 打开状态时的文案
+    checkedText: '',
+    // 关闭状态时的文案
+    uncheckedText: ''
+}
+
 export const SwitchWidget: WidgetInterface = {
-    name: '开关',
+    id: '7385a57c74e64243bf3915a2a3f43a3d',
+    name: '开关-✔',
     icon: 'widget-switch',
     tag: 'switch',
     enabled: {
@@ -10,7 +36,9 @@ export const SwitchWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
-    _config: {},
+    _config: {
+        arco: 'TientorConfigArcoSwitch'
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -18,5 +46,7 @@ export const SwitchWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
