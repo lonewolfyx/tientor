a626a406f732565659e327c948b5273da9cb3c68
lonewolfyx <877085893@qq.com>
2024-12-04 22:09:44 +0800

    feat：新增 Arco Transfer 穿梭框配置项

--- packages/tientor-configuration/src/arco/TransferWidget.vue
+++ packages/tientor-configuration/src/arco/TransferWidget.vue
@@ -1,9 +1,32 @@
 <template>
-
+    <a-form-item label="源选择框标题：" field="defaultValue">
+        <a-input v-model="queryForm._widgetProp[ui].title[0]" placeholder="请输入默认标题..."/>
+    </a-form-item>
+    <a-form-item label="目标选择框标题：" field="defaultValue">
+        <a-input v-model="queryForm._widgetProp[ui].title[1]" placeholder="请输入默认标题..."/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="简单模式：" help="选择即移动">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].simple"/>
+    </a-form-item>
+    <a-form-item label="单向模式：" help="仅可移动到目标选择框">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].oneWay"/>
+    </a-form-item>
+    <a-form-item label="搜索框：" field="showSearch">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showSearch"/>
+    </a-form-item>
+    <a-form-item label="全选勾选框：" field="showSelectAll">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showSelectAll"/>
+    </a-form-item>
 </template>
 
 <script setup>
+
 defineOptions({
     name: 'TientorConfigArcoTransfer'
 })
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex']);
+
 </script>
--- packages/tientor-widget/src/widget/TransferWidget.ts
+++ packages/tientor-widget/src/widget/TransferWidget.ts
@@ -1,8 +1,41 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const TransferOptions = Array(5).fill(undefined).map((_, index) => ({
+    value: `option${index + 1}`,
+    label: `Option ${index + 1}`
+}));
+
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: [],
+    // 穿梭框的数据
+    data: TransferOptions,
+    // 是否禁用
+    disabled: false,
+    // 是否开启简单模式（点击选项即移动）
+    simple: false,
+    // 是否开启单向模式（仅可移动到目标选择框）
+    oneWay: false,
+    // 是否显示搜索框
+    showSearch: false,
+    // 是否展示全选勾选框
+    showSelectAll: true,
+    // 源选择框和目标选择框的标题
+    title: ['Source', 'Target'],
+    // 源选择框的搜索框配置
+    sourceInputSearchProps: {
+        placeholder: '请输入你要搜索的内容'
+    },
+    // 目标选择框的搜索框配置
+    targetInputSearchProps: {
+        placeholder: '请输入你要搜索的内容'
+    }
+}
+
 export const TransferWidget: WidgetInterface = {
-    name: '穿梭框',
+    id: 'a9dc03a749bc4153bd15d5ba42cdf488',
+    name: '穿梭框-✔',
     icon: 'widget-transfer',
     tag: 'transfer',
     enabled: {
@@ -10,7 +43,9 @@ export const TransferWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
-    _config: {},
+    _config: {
+        arco: 'TientorConfigArcoTransfer'
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -18,5 +53,7 @@ export const TransferWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
