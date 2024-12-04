d2332f514c6556219364490210e0f6aa95484196
lonewolfyx <877085893@qq.com>
2024-12-04 22:39:49 +0800

    feat：新增 Arco Cascader 级联选择器配置项

--- packages/tientor-configuration/src/arco/CascaderWidget.vue
+++ packages/tientor-configuration/src/arco/CascaderWidget.vue
@@ -1,9 +1,75 @@
 <template>
-
+    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>
+    <DividerHeader title="组件设置"/>
+<!--    <a-form-item label="基础数据：">-->
+<!--        <a-button>-->
+<!--            <template #icon>-->
+<!--                <icon-edit/>-->
+<!--            </template>-->
+<!--            数据编辑-->
+<!--        </a-button>-->
+<!--    </a-form-item>-->
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="绑定值为路径：" field="pathMode">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].pathMode"/>
+    </a-form-item>
+    <a-form-item label="多选模式：" field="multiple">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].multiple"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="搜索模式：" field="allowSearch">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowSearch"/>
+    </a-form-item>
+    <a-form-item label="允许清除：" field="allowClear">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowClear"/>
+    </a-form-item>
+    <a-form-item label="下级触发方式：">
+        <a-radio-group type="button" v-model="queryForm._widgetProp[ui].expandTrigger">
+            <a-radio value="click">Click</a-radio>
+            <a-radio value="hover">Hover</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="提示文字：" field="placeholder">
+        <a-input v-model="queryForm._widgetProp[ui].placeholder" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="标签数量：" help="开启多选模式下选择的标签数量，0 则不限制">
+        <a-input-number v-model="queryForm._widgetProp[ui].maxTagCount" :min="0" mode="button"/>
+    </a-form-item>
+    <a-form-item label="记载中：" field="loading">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].loading"/>
+    </a-form-item>
+    <a-form-item label="严格模式：" field="checkStrictly">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].checkStrictly"/>
+    </a-form-item>
+    <a-form-item label="搜索延迟时间：">
+        <a-input-number v-model="queryForm._widgetProp[ui].searchDelay" :min="0" mode="button"/>
+    </a-form-item>
+    <a-form-item label="选项键值：" field="valueKey">
+        <a-input v-model="queryForm._widgetProp[ui].valueKey"/>
+    </a-form-item>
+    <a-form-item label="展开子菜单：" field="expandChild">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].expandChild"/>
+    </a-form-item>
+    <a-form-item label="标签内容不换行：" field="tagNowrap">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].tagNowrap"/>
+    </a-form-item>
 </template>
 
 <script setup>
+import DividerHeader from "../components/DividerHeader.vue";
+import TientorConfigArcoFormItem from "./FormItem.vue";
+
 defineOptions({
     name: 'TientorConfigArcoCascader'
 })
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex']);
 </script>
--- packages/tientor-widget/src/widget/CascaderWidget.ts
+++ packages/tientor-widget/src/widget/CascaderWidget.ts
@@ -1,8 +1,103 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const cascaderOptions = [
+    {
+        value: 'beijing',
+        label: 'Beijing',
+        children: [
+            {
+                value: 'chaoyang',
+                label: 'ChaoYang',
+                children: [
+                    {
+                        value: 'datunli',
+                        label: 'Datunli',
+                    },
+                ],
+            },
+            {
+                value: 'haidian',
+                label: 'Haidian',
+            },
+            {
+                value: 'dongcheng',
+                label: 'Dongcheng',
+            },
+            {
+                value: 'xicheng',
+                label: 'Xicheng',
+                children: [
+                    {
+                        value: 'jinrongjie',
+                        label: 'Jinrongjie',
+                    },
+                    {
+                        value: 'tianqiao',
+                        label: 'Tianqiao',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        value: 'shanghai',
+        label: 'Shanghai',
+        children: [
+            {
+                value: 'huangpu',
+                label: 'Huangpu',
+            },
+        ],
+    },
+];
+
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: '',
+    // 级联选择器的选项
+    options: cascaderOptions,
+    // 绑定值是否为路径
+    pathMode: false,
+    // 是否为多选状态（多选模式默认开启搜索）
+    multiple: false,
+    // 是否禁用
+    disabled: false,
+    // 选择框的大小
+    size: 'medium',
+    // 是否允许搜索
+    allowSearch: false,
+    // 是否允许清除
+    allowClear: false,
+    // 展开下一级的触发方式 'click' | 'hover'
+    expandTrigger: 'click',
+    // 占位符
+    placeholder: '',
+    // 多选模式下，最多显示的标签数量。0 表示不限制
+    maxTagCount: 0,
+    // 是否为加载中状态
+    loading: false,
+    // // 下拉菜单的触发器属性
+    // triggerProps: {}, 有点多 后期维护，https://github.com/arco-design/arco-design-vue/blob/main/packages/web-vue/components/cascader/cascader.vue#L326C5-L326C17
+    // 是否开启严格选择模式
+    checkStrictly: false,
+    // // 搜索下拉菜单中的选项是否仅展示标签
+    // searchOptionOnlyLabel: false,
+    // 触发搜索事件的延迟时间
+    searchDelay: 500,
+    // // 自定义 `CascaderOption` 中的字段
+    // fieldNames:{},有点多 后期维护 https://github.com/arco-design/arco-design-vue/blob/main/packages/web-vue/components/cascader/cascader.vue#L383
+    // 用于确定选项键值的属性名
+    valueKey: 'value',
+    // 是否展开子菜单
+    expandChild: false,
+    // 标签内容不换行
+    tagNowrap: false,
+}
+
 export const CascaderWidget: WidgetInterface = {
-    name: '级联选择器',
+    id: '42cac5cddce04e9894bb50c576063a94',
+    name: '级联选择器-✔',
     icon: 'widget-cascader',
     tag: 'cascader',
     enabled: {
@@ -10,7 +105,9 @@ export const CascaderWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
-    _config: {},
+    _config: {
+        arco: 'TientorConfigArcoCascader'
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -18,5 +115,7 @@ export const CascaderWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 };
