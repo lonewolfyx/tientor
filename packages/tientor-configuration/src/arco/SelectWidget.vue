080cbe8a53f942fce2f3396cf4db600ebed75a69
lonewolfyx <877085893@qq.com>
2024-12-04 20:52:21 +0800

    feat：新增 Arco Select 选择器配置项

--- packages/tientor-configuration/src/arco/SelectWidget.vue
+++ packages/tientor-configuration/src/arco/SelectWidget.vue
@@ -1,9 +1,139 @@
 <template>
-
+    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>
+    <DividerHeader title="组件设置"/>
+    <a-form-item label="默认值：" field="defaultValue">
+        <a-input type="round" v-model="queryForm._widgetProp[ui].defaultValue"/>
+    </a-form-item>
+    <a-form-item label="基础数据：">
+        <a-button>
+            <template #icon>
+                <icon-edit/>
+            </template>
+            数据编辑
+        </a-button>
+    </a-form-item>
+    <a-form-item label="多选模式：" field="multiple">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].multiple"/>
+    </a-form-item>
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="选项键值：" field="valueKey">
+        <a-input v-model="queryForm._widgetProp[ui].valueKey"/>
+    </a-form-item>
+    <a-form-item label="提示文字：" field="placeholder">
+        <a-input v-model="queryForm._widgetProp[ui].placeholder" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="允许清空输入框：" field="allowClear">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowClear"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="允许创建：" field="allowCreate">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowCreate"/>
+    </a-form-item>
+    <a-form-item
+        label="标签数量："
+        help="开启多选模式后，可设置的标签选择数量"
+        :disabled="!queryForm._widgetProp[ui].multiple"
+    >
+        <a-input-number v-model="queryForm._widgetProp[ui].maxTagCount" :min="0"/>
+    </a-form-item>
+    <a-form-item
+        label="多选数量："
+        help="开启多选模式后，可设置的选择数量"
+        :disabled="!queryForm._widgetProp[ui].multiple"
+    >
+        <a-input-number mode="button" v-model="queryForm._widgetProp[ui].limit" :min="0"/>
+    </a-form-item>
+    <a-form-item label="边框样式：" field="bordered">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].bordered"/>
+    </a-form-item>
+    <a-form-item label="默认第一个：" help="在无值时默认选择第一个选项">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].defaultActiveFirstOption"/>
+    </a-form-item>
+    <a-form-item label="隐藏时销毁：" field="unmountOnClose">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].unmountOnClose"/>
+    </a-form-item>
+    <a-form-item label="额外显示：" help="在下拉菜单中显示额外选项">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showExtraOptions"/>
+    </a-form-item>
+    <a-form-item label="允许搜索：" field="allowSearch">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowSearch"/>
+    </a-form-item>
+    <a-form-item label="搜索延迟时间：" help="触发搜索事件的延迟时间，单位毫秒"
+                 :disabled="!queryForm._widgetProp[ui].allowSearch">
+        <a-input-number mode="button" v-model="queryForm._widgetProp[ui].searchDelay"/>
+    </a-form-item>
+    <a-form-item label="是否加载中：" field="loading">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].loading"/>
+    </a-form-item>
+    <a-form-item label="显示 Header：" help="空状态时是否显示 header">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showHeaderOnEmpty"/>
+    </a-form-item>
+    <a-form-item label="显示 footer：" help="空状态时是否显示 footer">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showFooterOnEmpty"/>
+    </a-form-item>
+    <a-form-item label="标签换行：" field="tagNowrap">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].tagNowrap"/>
+    </a-form-item>
+    <DividerHeader title="虚拟滚动设置"/>
+    <a-form-item label="虚拟滚动：">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].scrollbar"/>
+    </a-form-item>
+    <a-form-item label="高度：" :disabled="!queryForm._widgetProp[ui].scrollbar">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.height"
+            :min="0"
+            mode="button"
+            placeholder="默认虚拟列表高度"
+        />
+    </a-form-item>
+    <a-form-item label="固定高度：" :disabled="!queryForm._widgetProp[ui].scrollbar">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].virtualListProps.fixedSize"/>
+    </a-form-item>
+    <a-form-item label="默认数量：" help="可是窗口所呈现数量" :disabled="!queryForm._widgetProp[ui].scrollbar">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.threshold"
+            :min="0"
+            mode="button"
+        />
+    </a-form-item>
+    <a-form-item label="预估高度：" help="元素高度不固定时的预估高度" :disabled="!queryForm._widgetProp[ui].scrollbar">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.estimatedSize"
+            :min="0"
+            mode="button"
+        />
+    </a-form-item>
+    <a-form-item label="元素数量：" help="视口边界外提前挂载的元素数量" :disabled="!queryForm._widgetProp[ui].scrollbar">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.buffer"
+            :min="0"
+            mode="button"
+        />
+    </a-form-item>
 </template>
 
 <script setup>
+import DividerHeader from "../components/DividerHeader.vue";
+import TientorConfigArcoFormItem from "./FormItem.vue";
+
 defineOptions({
     name: 'TientorConfigArcoSelect'
 })
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex']);
+
+const data = [{
+    key: '1',
+    valueName: 'sadasd',
+    value: 'Jane Doe',
+    label: 23000,
+}]
 </script>
--- packages/tientor-widget/src/widget/SelectWidget.ts
+++ packages/tientor-widget/src/widget/SelectWidget.ts
@@ -1,7 +1,106 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: '',
+    // 选项数据
+    options: [
+        {
+            isGroup: true,
+            label: '1',
+            options: [
+                {
+                    label: '1-1',
+                    value: '1-1'
+                },
+                {
+                    title: '1-2',
+                    value: '1-2'
+                }
+            ]
+        },
+        {
+            isGroup: true,
+            label: '1',
+            options: [
+                {
+                    label: '1-1',
+                    value: '2-1'
+                },
+                {
+                    label: '1-2-2',
+                    value: '2-2'
+                }
+            ]
+        }
+    ],
+    // 是否开启多选模式
+    multiple: false,
+    // 选择框的大小
+    size: 'medium',
+    // 占位符
+    placeholder: '',
+    // 是否为加载中状态
+    loading: false,
+    // 是否禁用
+    disabled: false,
+    // 是否允许清空
+    allowClear: false,
+    // 是否允许搜索
+    allowSearch: false,
+    // 是否允许创建
+    allowCreate: false,
+    // 多选模式下，最多显示的标签数量。0 表示不限制
+    maxTagCount: 0,
+    // 是否显示输入框的边框
+    bordered: true,
+    // 是否在无值时默认选择第一个选项
+    defaultActiveFirstOption: true,
+    // 是否在下拉菜单关闭时销毁元素
+    unmountOnClose: false,
+    // 是否在下拉菜单中显示额外选项
+    showExtraOptions: true,
+    // 用于确定选项键值的属性名
+    valueKey: 'value',
+    // 触发搜索事件的延迟时间
+    searchDelay: 500,
+    // 多选时最多的选择个数
+    limit: 0,
+    // 自定义 `SelectOptionData` 中的字段
+    fieldNames: {
+        // 选项值
+        value: 'value',
+        // 选项内容
+        label: 'label',
+        // 是否禁用
+        disabled: false
+    },
+    // 是否开启虚拟滚动条
+    scrollbar: true,
+    // 传递虚拟列表属性，传入此参数以开启虚拟滚动，[VirtualListProps](#VirtualListProps)
+    virtualListProps: {
+        // 可视区域高度
+        height: NaN,
+        // 开启虚拟滚动的元素数量阈值，当数据数量小于阈值时不会开启虚拟滚动。
+        threshold: 100,
+        // 元素高度是否是固定的
+        fixedSize: false,
+        // 元素高度不固定时的预估高度
+        estimatedSize: 100,
+        // 视口边界外提前挂载的元素数量
+        buffer: 10
+    },
+    // 空状态时是否显示header
+    showHeaderOnEmpty: false,
+    // 空状态时是否显示footer
+    showFooterOnEmpty: false,
+    // 标签内容不换行
+    tagNowrap: false
+
+}
 export const SelectWidget: WidgetInterface = {
+    id: '0a73a5ca095b4763b531bf71f54bdf18',
     name: '选择器',
     icon: 'widget-select',
     tag: 'select',
@@ -10,7 +109,9 @@ export const SelectWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
-    _config: {},
+    _config: {
+        arco: 'TientorConfigArcoSelect',
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -18,5 +119,7 @@ export const SelectWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
