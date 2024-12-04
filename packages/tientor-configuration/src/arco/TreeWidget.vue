74e4a14d63dac2145499287bd6b0b7eca9ed1a53
lonewolfyx <877085893@qq.com>
2024-12-04 17:22:57 +0800

    feat：新增 Arco Tree 配置项

--- packages/tientor-configuration/src/arco/TreeWidget.vue
+++ packages/tientor-configuration/src/arco/TreeWidget.vue
@@ -1,9 +1,124 @@
 <template>
+    <TientorConfigArcoFormItem :ui="ui" :queryForm="queryForm"/>
+    <DividerHeader title="组件设置"/>
+    <!--    <a-form-item label="基础数据：">-->
+    <!--        {{queryForm._widgetProp[ui].data}}-->
 
+    <!--        <a-button type="primary">-->
+    <!--            <template #icon>-->
+    <!--                <icon-edit />-->
+    <!--            </template>-->
+    <!--            数据编辑-->
+    <!--        </a-button>-->
+    <!--    </a-form-item>-->
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="占据一行：" field="blockNode">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].blockNode"/>
+    </a-form-item>
+    <a-form-item label="展开父节点：" field="defaultExpandAll">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].defaultExpandAll"/>
+    </a-form-item>
+    <a-form-item label="多选节点：" field="multiple">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].multiple"/>
+    </a-form-item>
+    <a-form-item label="节点复选框：" field="checkable">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].checkable"/>
+    </a-form-item>
+    <a-form-item label="父子关联：" field="checkStrictly" help="是否取消父子节点关联">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].checkStrictly"/>
+    </a-form-item>
+    <a-form-item label="回填方式：">
+        <a-radio-group type="button" v-model="queryForm._widgetProp[ui].checkedStrategy">
+            <a-radio value="all">所有</a-radio>
+            <a-radio value="parent">父子节点</a-radio>
+            <a-radio value="child">子节点</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="连接线：" field="showLine">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showLine"/>
+    </a-form-item>
+    <a-form-item label="拖拽节点：" field="draggable">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].draggable"/>
+    </a-form-item>
+    <a-form-item label="节点父节点：" field="defaultExpandSelected" help="是否默认展开已选中节点的父节点">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].defaultExpandSelected"/>
+    </a-form-item>
+    <a-form-item label="复选框父节点：" field="defaultExpandChecked" help="是否默认展开已选中复选框节点的父节点">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].defaultExpandChecked"/>
+    </a-form-item>
+    <a-form-item label="展开父节点：" field="autoExpandParent" help="是否自动展开已展开节点的父节点">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].autoExpandParent"/>
+    </a-form-item>
+    <DividerHeader title="节点数据字段名设置"/>
+    <a-form-item label="索引值：" help="指定 key 在 TreeNodeData 中的字段名">
+        <a-input v-model="queryForm._widgetProp[ui].fieldNames.key"/>
+    </a-form-item>
+    <a-form-item label="展示值：" help="指定 title 在 TreeNodeData 中的字段名">
+        <a-input v-model="queryForm._widgetProp[ui].fieldNames.title"/>
+    </a-form-item>
+    <a-form-item label="子节点值：" help="指定 children 在 TreeNodeData 中的字段名">
+        <a-input v-model="queryForm._widgetProp[ui].fieldNames.children"/>
+    </a-form-item>
+
+    <DividerHeader title="虚拟列表设置"/>
+    <a-form-item label="虚拟列表：" field="virtual">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].virtual" @change="virtualHandle"/>
+    </a-form-item>
+    <a-form-item label="高度：">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.height"
+            :min="0"
+            mode="button"
+            placeholder="默认虚拟列表高度"
+        />
+    </a-form-item>
+    <a-form-item label="固定高度：">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].virtualListProps.fixedSize"/>
+    </a-form-item>
+    <a-form-item label="默认数量：" help="可是窗口所呈现数量">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.threshold"
+            :min="0"
+            mode="button"
+        />
+    </a-form-item>
+    <a-form-item label="预估高度：" help="元素高度不固定时的预估高度">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.estimatedSize"
+            :min="0"
+            mode="button"
+        />
+    </a-form-item>
+    <a-form-item label="元素数量：" help="视口边界外提前挂载的元素数量">
+        <a-input-number
+            v-model="queryForm._widgetProp[ui].virtualListProps.buffer"
+            :min="0"
+            mode="button"
+        />
+    </a-form-item>
 </template>
 
 <script setup>
+import DividerHeader from "../components/DividerHeader.vue";
+import TientorConfigArcoFormItem from "./FormItem.vue";
+
 defineOptions({
     name: 'TientorConfigArcoTree'
 })
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex']);
+
+const virtualHandle = (checked) => {
+    if (checked) {
+        queryForm._widgetProp[ui].virtualListProps.height = 500;
+    } else {
+        queryForm._widgetProp[ui].virtualListProps.height = NaN;
+    }
+}
 </script>
--- packages/tientor-widget/src/widget/TreeWidget.ts
+++ packages/tientor-widget/src/widget/TreeWidget.ts
@@ -1,8 +1,180 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: [
+        {
+            title: 'Trunk 0-0',
+            key: '0-0',
+            children: [
+                {
+                    title: 'Leaf',
+                    key: '0-0-1',
+                },
+                {
+                    title: 'Branch 0-0-2',
+                    key: '0-0-2',
+                    disabled: true,
+                    children: [
+                        {
+                            title: 'Leaf',
+                            key: '0-0-2-1'
+                        },
+                        {
+                            title: 'Leaf',
+                            key: '0-0-2-2',
+                            disableCheckbox: true
+                        }
+                    ]
+                },
+            ],
+        },
+        {
+            title: 'Trunk 0-1',
+            key: '0-1',
+            children: [
+                {
+                    title: 'Branch 0-1-1',
+                    key: '0-1-1',
+                    children: [
+                        {
+                            title: 'Leaf ',
+                            key: '0-1-1-1',
+                        },
+                        {
+                            title: 'Leaf ',
+                            key: '0-1-1-2',
+                        },
+                    ]
+                },
+                {
+                    title: 'Leaf',
+                    key: '0-1-2',
+                },
+            ],
+        },
+    ],
+    data: [
+        {
+            title: 'Trunk 0-0',
+            key: '0-0',
+            children: [
+                {
+                    title: 'Leaf',
+                    key: '0-0-1',
+                },
+                {
+                    title: 'Branch 0-0-2',
+                    key: '0-0-2',
+                    disabled: true,
+                    children: [
+                        {
+                            title: 'Leaf',
+                            key: '0-0-2-1'
+                        },
+                        {
+                            title: 'Leaf',
+                            key: '0-0-2-2',
+                            disableCheckbox: true
+                        }
+                    ]
+                },
+            ],
+        },
+        {
+            title: 'Trunk 0-1',
+            key: '0-1',
+            children: [
+                {
+                    title: 'Branch 0-1-1',
+                    key: '0-1-1',
+                    children: [
+                        {
+                            title: 'Leaf ',
+                            key: '0-1-1-1',
+                        },
+                        {
+                            title: 'Leaf ',
+                            key: '0-1-1-2',
+                        },
+                    ]
+                },
+                {
+                    title: 'Leaf',
+                    key: '0-1-2',
+                },
+            ],
+        },
+    ],
+    // 尺寸
+    size: 'medium',
+    // 节点是否占据一行
+    blockNode: false,
+    // 是否默认展开父节点
+    defaultExpandAll: false,
+    // 是否支持多选
+    multiple: false,
+    // 是否在节点前添加复选框
+    checkable: false,
+    // 是否支持选择
+    selectable: true,
+    // 是否取消父子节点关联
+    checkStrictly: false,
+    // 定制回填方式
+    checkedStrategy: 'all',
+    // 指定节点数据中的字段名
+    // fieldNames:{},
+    fieldNames: {
+        // 指定 key 在 TreeNodeData 中的字段名
+        key: 'key',
+        // 指定 title 在 TreeNodeData 中的字段名
+        title: 'title',
+        // // 指定 disabled 在 TreeNodeData 中的字段名
+        // disabled: '',
+        // 指定 children 在 TreeNodeData 中的字段名
+        children: 'children',
+        // // 指定 isLeaf 在 TreeNodeData 中的字段名
+        // isLeaf: '',
+        // // 指定 disableCheckbox 在 TreeNodeData 中的字段名
+        // disableCheckbox: '',
+        // // 指定 checkable 在 TreeNodeData 中的字段名
+        // checkable: '',
+        // // 指定 icon 在 TreeNodeData 中的字段名
+        // icon: ''
+    },
+    // 是否展示连接线
+    showLine: false,
+    // 是否可以拖拽
+    draggable: false,
+    // 是否开启虚拟列表
+    virtual: false,
+    // 传递虚拟列表属性，传入此参数以开启虚拟滚动，[VirtualListProps](#VirtualListProps)
+    virtualListProps: {
+        // 可视区域高度
+        height: NaN,
+        // 开启虚拟滚动的元素数量阈值，当数据数量小于阈值时不会开启虚拟滚动。
+        threshold: 100,
+        // 元素高度是否是固定的
+        fixedSize: true,
+        // 元素高度不固定时的预估高度
+        estimatedSize: 100,
+        // 视口边界外提前挂载的元素数量
+        buffer: 10
+    },
+    // 是否默认展开已选中节点的父节点
+    defaultExpandSelected: false,
+    // 是否默认展开已选中复选框节点的父节点
+    defaultExpandChecked: false,
+    // 是否自动展开已展开节点的父节点
+    autoExpandParent: true,
+    // 是否开启展开时的过渡动效
+    animation: false
+}
+
 export const TreeWidget: WidgetInterface = {
-    name: '树形控件',
+    id: 'f90246aaecca478886634d935a337f2b',
+    name: '树形控件-✔',
     icon: 'widget-tree',
     tag: 'tree',
     enabled: {
@@ -10,7 +182,9 @@ export const TreeWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
-    _config: {},
+    _config: {
+        arco: 'TientorConfigArcoTree'
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -18,5 +192,7 @@ export const TreeWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
