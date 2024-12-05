78e8a6fc9740e174e01200bd887b912873eda449
lonewolfyx <877085893@qq.com>
2024-12-05 14:15:43 +0800

    feat：新增 Arco TreeSelect 树形选择器配置项

--- packages/tientor-configuration/src/arco/TreeSelectWidget.vue
+++ packages/tientor-configuration/src/arco/TreeSelectWidget.vue
@@ -1,9 +1,88 @@
 <template>
 
+<!--    <a-form-item label="基础数据：">-->
+<!--        <a-button>-->
+<!--            <template #icon>-->
+<!--                <icon-edit/>-->
+<!--            </template>-->
+<!--            数据编辑-->
+<!--        </a-button>-->
+<!--    </a-form-item>-->
+    <a-form-item label="提示文字：" field="placeholder">
+        <a-input v-model="queryForm._widgetProp[ui].placeholder" placeholder="请输入默认提示文字..."/>
+    </a-form-item>
+    <a-form-item label="边框样式：" field="bordered">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].border"/>
+    </a-form-item>
+    <a-form-item label="组件尺寸：">
+        <a-radio-group type="button" size="small" v-model="queryForm._widgetProp[ui].size">
+            <a-radio value="mini">mini</a-radio>
+            <a-radio value="small">小</a-radio>
+            <a-radio value="medium">默认</a-radio>
+            <a-radio value="large">大</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="多选：" field="multiple">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].multiple"/>
+    </a-form-item>
+    <a-form-item
+        label="标签数量："
+        help="开启多选模式后，可设置的标签选择数量"
+        :disabled="!queryForm._widgetProp[ui].multiple"
+    >
+        <a-input-number v-model="queryForm._widgetProp[ui].maxTagCount" :min="0"/>
+    </a-form-item>
+    <a-form-item label="清空选择框：" field="allowClear">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowClear"/>
+    </a-form-item>
+    <a-form-item label="搜索模式：" field="allowSearch">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].allowSearch"/>
+    </a-form-item>
+    <a-form-item label="禁用组件：" field="disabled">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].disabled"/>
+    </a-form-item>
+    <a-form-item label="加载中：" field="loading">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].loading"/>
+    </a-form-item>
+    <a-form-item label="复选框：">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].treeCheckable"/>
+    </a-form-item>
+    <a-form-item label="父子节点关联：">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].treeCheckStrictly"/>
+    </a-form-item>
+    <a-form-item label="定制回显：">
+        <a-radio-group type="button" v-model="queryForm._widgetProp[ui].treeCheckedStrategy">
+            <a-radio value="all">all</a-radio>
+            <a-radio value="parent">parent</a-radio>
+            <a-radio value="child">child</a-radio>
+        </a-radio-group>
+    </a-form-item>
+    <a-form-item label="虚拟滚动：">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].scrollbar"/>
+    </a-form-item>
+    <a-form-item label="显示 Header：" help="空状态时是否显示 header">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showHeaderOnEmpty"/>
+    </a-form-item>
+    <a-form-item label="显示 footer：" help="空状态时是否显示 footer">
+        <a-switch type="round" v-model="queryForm._widgetProp[ui].showFooterOnEmpty"/>
+    </a-form-item>
+    <DividerHeader title="节点数据字段名设置"/>
+    <a-form-item label="索引值：" help="指定 key 在 TreeNodeData 中的字段名">
+        <a-input v-model="queryForm._widgetProp[ui].fieldNames.key"/>
+    </a-form-item>
+    <a-form-item label="展示值：" help="指定 title 在 TreeNodeData 中的字段名">
+        <a-input v-model="queryForm._widgetProp[ui].fieldNames.title"/>
+    </a-form-item>
+    <a-form-item label="子节点值：" help="指定 children 在 TreeNodeData 中的字段名">
+        <a-input v-model="queryForm._widgetProp[ui].fieldNames.children"/>
+    </a-form-item>
 </template>
 
 <script setup>
+import DividerHeader from "../components/DividerHeader.vue";
+
 defineOptions({
     name: 'TientorConfigArcoTreeSelect'
 })
+const {ui, queryForm, selectIndex} = defineProps(['ui', 'queryForm', 'selectIndex']);
 </script>
--- packages/tientor-widget/src/widget/TreeSelectWidget.ts
+++ packages/tientor-widget/src/widget/TreeSelectWidget.ts
@@ -1,8 +1,96 @@
 import {WidgetInterface} from "../types/widget";
 import {ArcoItemProps} from "../item/arco";
 
+const treeData = [
+    {
+        key: 'node1',
+        title: 'Trunk',
+        disabled: true,
+        children: [
+            {
+                key: 'node2',
+                title: 'Leaf',
+            },
+        ],
+    },
+    {
+        key: 'node3',
+        title: 'Trunk2',
+        children: [
+            {
+                key: 'node4',
+                title: 'Leaf',
+            },
+            {
+                key: 'node5',
+                title: 'Leaf',
+            },
+        ],
+    },
+];
+
+
+const ArcoWidgetProps = {
+    // 默认值
+    defaultValue: '',
+    // 数据
+    data: treeData,
+    // 是否禁用
+    disabled: false,
+    // 是否为加载中状态
+    loading: false,
+    // 选择框的大小
+    size: 'medium',
+    // 是否显示边框
+    border: true,
+    // 是否允许搜索
+    allowSearch: false,
+    // 是否允许清除
+    allowClear: false,
+    // 提示文案
+    placeholder: '',
+    // 是否支持多选
+    multiple: false,
+    // 最多显示的标签数量，仅在多选模式有效
+    maxTagCount: 0,
+    // 指定节点数据中的字段名
+    fieldNames: {
+        // 指定 key 在 TreeNodeData 中的字段名
+        key: 'key',
+        // 指定 title 在 TreeNodeData 中的字段名
+        title: 'title',
+        // // 指定 disabled 在 TreeNodeData 中的字段名
+        // disabled: '',
+        // 指定 children 在 TreeNodeData 中的字段名
+        children: 'children',
+        // // 指定 isLeaf 在 TreeNodeData 中的字段名
+        // isLeaf: '',
+        // // 指定 disableCheckbox 在 TreeNodeData 中的字段名
+        // disableCheckbox: '',
+        // // 指定 checkable 在 TreeNodeData 中的字段名
+        // checkable: '',
+        // // 指定 icon 在 TreeNodeData 中的字段名
+        // icon: ''
+    },
+    // // 设置value格式。默认是string，设置为true时候，value格式为： { label: string, value: string }
+    // labelInValue: false,
+    // 是否展示复选框
+    treeCheckable: false,
+    // 父子节点是否关联
+    treeCheckStrictly: false,
+    // 定制回显方式 'all' | 'parent' | 'child'
+    treeCheckedStrategy: 'all',
+    // 是否开启虚拟滚动条
+    scrollbar: true,
+    // 空状态时是否显示header
+    showHeaderOnEmpty: false,
+    // 空状态时是否显示footer
+    showFooterOnEmpty: false
+}
+
 export const TreeSelectWidget: WidgetInterface = {
-    name: '树形选择器',
+    id: '126394f0f76945d9a0a8828288088d76',
+    name: '树形选择器-✔',
     icon: 'widget-tree-select',
     tag: 'treeSelect',
     enabled: {
@@ -10,7 +98,9 @@ export const TreeSelectWidget: WidgetInterface = {
         antv: true,
         arco: true
     },
-    _config: {},
+    _config: {
+        arco: 'TientorConfigArcoTreeSelect',
+    },
     _itemProps: {
         arco: {
             ...ArcoItemProps, ...{
@@ -18,5 +108,7 @@ export const TreeSelectWidget: WidgetInterface = {
             }
         },
     },
-    _widgetProp: {}
+    _widgetProp: {
+        arco: {...ArcoWidgetProps}
+    }
 }
