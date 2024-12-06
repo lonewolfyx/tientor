6952726c139c830838651687e033d2c23b5607ab
lonewolfyx <877085893@qq.com>
2024-12-06 15:18:36 +0800

    fix：打包优化

--- packages/tientor-engine/vite.config.js
+++ packages/tientor-engine/vite.config.js
@@ -48,5 +48,39 @@ export default defineConfig({
                 api: 'modern-compiler', // or "modern", "legacy"
             },
         },
+    },
+    // 打包处理
+    build: {
+        minify: true,
+        cssCodeSplit: true,
+        chunkSizeWarningLimit: 500,
+
+        terserOptions: {
+            compress: {
+                drop_console: true, // 删除console语句
+                drop_debugger: true, // 删除debugger
+            },
+            format: {
+                comments: false, // 移除注释
+            },
+        },
+
+        rollupOptions: {
+            output: {
+                // 最小化拆分包
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+                // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+                entryFileNames: `assets/js/[name]/[hash].js`,
+                // 用于命名代码拆分时创建的共享块的输出命名
+                chunkFileNames: `assets/js/[name]/[hash].js`,
+                // 用于输出静态资源的命名，[ext]表示文件扩展名
+                assetFileNames: `assets/[ext]/[name]/[hash].[ext]`,
+
+            }
+        }
     }
 })
